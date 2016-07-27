d23d5206324eb0c8b1820eb18a839f88e137497f
Chris Mytton <chrism@mysociety.org>
2016-07-27 16:52:22 +0100

    Remove broken tests

--- test/scraped_page_archive_test.rb
+++ test/scraped_page_archive_test.rb
@@ -4,24 +4,4 @@ class ScrapedPageArchiveTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::ScrapedPageArchive::VERSION
   end
-
-  class ArchiverTest < Minitest::Test
-    def mock_archiver
-      @mock_archiver ||= Minitest::Mock.new
-    end
-
-    def setup
-      ScrapedPageArchive.archiver = mock_archiver
-    end
-
-    def test_archiver_is_called
-      example_url = 'http://example.org'
-      mock_archiver.expect :archive!, nil do |url, &block|
-        assert_equal example_url, url
-        assert_equal 'block result', block.call
-      end
-      ScrapedPageArchive.archive!(example_url) { |_| 'block result' }
-      mock_archiver.verify
-    end
-  end
 end
