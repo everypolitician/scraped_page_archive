846753952ee0c228ce34e03e0b626a56f02dbb3b
Chris Mytton <chrism@mysociety.org>
2016-07-29 12:02:22 +0100

    Add with_tmp_dir helper method for tests
    This makes it simpler to create a tmp directory and chdir to it in the
    tests, which is handy when testing git related things.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,11 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'scraped_page_archive'
 
 require 'minitest/autorun'
+
+class Minitest::Spec
+  def with_tmp_dir(&block)
+    Dir.mktmpdir do |tmp_dir|
+      Dir.chdir(tmp_dir, &block)
+    end
+  end
+end
