58d652704d5a36b6909299ebca394410276b0642
Chris Mytton <chrism@mysociety.org>
2016-07-29 12:02:58 +0100

    Add failing tests for git_remote_get_url_origin functionality

--- test/scraped_page_archive_test.rb
+++ test/scraped_page_archive_test.rb
@@ -4,4 +4,32 @@ describe ScrapedPageArchive do
   it 'has a version number' do
     refute_nil ::ScrapedPageArchive::VERSION
   end
+
+  describe '#git_remote_get_url_origin' do
+    describe 'in a git repo' do
+      it 'returns the origin url of a git repo' do
+        with_tmp_dir do
+          remote_url = 'https://git.example.org/foo.git'
+          `git init`
+          `git remote add origin #{remote_url}`
+          assert_equal remote_url, ScrapedPageArchive.git_remote_get_url_origin
+        end
+      end
+
+      it 'returns nil if there is no origin remote' do
+        with_tmp_dir do
+          `git init`
+          assert_nil ScrapedPageArchive.git_remote_get_url_origin
+        end
+      end
+    end
+
+    describe 'no git repo' do
+      it 'returns nil' do
+        with_tmp_dir do
+          assert_nil ScrapedPageArchive.git_remote_get_url_origin
+        end
+      end
+    end
+  end
 end
