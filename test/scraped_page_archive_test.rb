e0d7e676782f9e74925998a17d053b87aa7a1c86
Chris Mytton <chrism@mysociety.org>
2016-07-29 11:38:39 +0100

    Switch tests to use Minitest::Spec
    This makes it easier to write tests using string descriptions rather
    than awkward method names.

--- test/scraped_page_archive_test.rb
+++ test/scraped_page_archive_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
-class ScrapedPageArchiveTest < Minitest::Test
-  def test_that_it_has_a_version_number
+describe ScrapedPageArchive do
+  it 'has a version number' do
     refute_nil ::ScrapedPageArchive::VERSION
   end
 end
