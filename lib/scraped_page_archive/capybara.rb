f16a5186a7a9d0aa610de56f431be9a26d0af294
Struan Donald <struan@exo.org.uk>
2016-08-12 16:12:22 +0100

    override command method of Browser object
    rather than just overriding the visit method override the command method
    which is called by everything so we catch all the page fetches.
    
    Because this is also called by things like find we have a list of
    methods that we return on straight away rather than trying to save the
    current page. Without this scraping becomes quite slow.

--- lib/scraped_page_archive/capybara.rb
+++ lib/scraped_page_archive/capybara.rb
@@ -3,8 +3,8 @@ require 'capybara/poltergeist'
 require 'scraped_page_archive'
 
 module Capybara::Poltergeist
-  class Driver
-    alias __visit visit
+  class Browser
+    alias __command command
 
     def sha_url(url)
       Digest::SHA1.hexdigest url
@@ -23,6 +23,7 @@ module Capybara::Poltergeist
     end
 
     def get_details(url)
+      status_code = page.status_code
       {
         'request' => {
           'method' => 'get', # assume this as no way to access it
@@ -30,30 +31,35 @@ module Capybara::Poltergeist
         },
         'response' => {
           'status' => {
-            'message' => page.status_code == 200 ? 'OK' : 'NOT OK',
-            'code' => page.status_code
+            'message' => status_code == 200 ? 'OK' : 'NOT OK',
+            'code' => status_code
           },
           'date' => [ page.response_headers['Date'] ]
         }
       }
     end
 
-    def save_request(url)
+    def save_request(html, details, url)
       html_path, yaml_path = get_paths(url)
 
       File.open(html_path,"w") do |f|
-        f.write(page.html)
+        f.write(html)
       end
       File.open(yaml_path,"w") do |f|
-        f.write(YAML.dump(get_details(url)))
+        f.write(YAML.dump(details))
       end
     end
 
-    def visit(url)
+    def command(name, *args)
+      result = __command(name, *args)
+      # we skip these methods because they are called a lot, don't cause the page
+      # to change and having record round them slows things down quite a bit.
+      return result if ['tag_name', 'visible', 'property', 'find', 'body', 'set_js_errors', 'current_url', 'status_code', 'response_headers'].include?(name)
+      current_url = page.current_url.to_s
       ScrapedPageArchive.record do
-        __visit(url)
-        save_request(page.current_url.to_s)
+        save_request(page.html, get_details(current_url), current_url)
       end
+      result
     end
   end
 end
